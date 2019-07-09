93514b885b98bb3c32e14c022f6dbdaef72677b0
v_shiyisen <v_shiyisen@baidu.com>
2019-07-09 12:26:10 +0800

    two

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:shiyisen321/Esen.github.io.git master
+git push -f git@github.com:shiyisen321/shiyisen321.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shiyisen321/Esen.github.io.git master:gh-pages
+# git push -f git@github.com:shiyisen321/Esen.github.io.git master:gh-pages
 
 cd -
 
--- docs/README.md
+++ docs/README.md
@@ -7,5 +7,5 @@ features:
 footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
 ## 🎨大前端
-- [element源码分析](/elementUI/knownFramework/)
+- [element源码分析](/element-ui-2019-07-04/)
 
