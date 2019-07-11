ccb8f8147c9a993ed85b57d84028fed114d22856
v_shiyisen <v_shiyisen@baidu.com>
2019-07-11 21:21:38 +0800

    优化博客部署脚本

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 npm run build
 
-cd docs/.vuepress/dist
+cd docs/.vuepress/dist && touch README.md && echo -e "博客地址：https://shiyisen321.github.io \n # 欢迎大家评论收藏" >> README.md
 
 git init
 git add -A
