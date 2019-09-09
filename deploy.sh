c35adf31b12de80c4ae3fc3134242e7fabdccd1b
v_shiyisen <v_shiyisen@baidu.com>
2019-09-09 11:32:09 +0800

    修改域名

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 npm run build
 
-cd docs/.vuepress/dist && touch README.md && echo -e "博客地址：https://shiyisen321.github.io \n # 欢迎大家评论收藏" >> README.md
+cd docs/.vuepress/dist && touch README.md && echo -e "博客地址：https://YisenFE.github.io \n # 欢迎大家评论收藏" >> README.md
 
 git init
 git add -A
@@ -13,7 +13,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:shiyisen321/shiyisen321.github.io.git master
 
 # 自动构建
-git push -f https://${access_token}@github.com/shiyisen321/shiyisen321.github.io.git master
+git push -f https://${access_token}@github.com/YisenFE/YisenFE.github.io.git master
 
 cd -
 
